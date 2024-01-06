8cf80cd1f5a69c6064e2dc97a63457d2d74af7c7
Brian Hie <blhie1@gmail.com>
2024-01-06 19:09:31 +0000

    fix charlevel

--- pretrain_gpt_charlevel.py
+++ pretrain_gpt_charlevel.py
@@ -1,12 +1,15 @@
 from pretrain_gpt import (
     pretrain,
-    train_valid_test_datsets_provider,
+    train_valid_test_datasets_provider,
     model_provider,
     ModelType,
     forward_step,
 )
 
 if __name__ == "__main__":
+
+    # Temporary for transition to core datasets
+    train_valid_test_datasets_provider.is_distributed = True
 
     pretrain(train_valid_test_datasets_provider,
              model_provider,
